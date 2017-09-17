980c791c653d3dcb7bc8f6391f498fe1a68c01ce
Zarak Mahmud <zarak.mahmud@utoronto.ca>
2017-09-17 23:22:18 +0500

    Rename to vector for clarity

--- python/perceptron.py
+++ python/perceptron.py
@@ -40,40 +40,23 @@ class Data(object):
     def X(self):
         return self.__X
 
-    def _initialize_points(self):
-        """Random points"""
-        dim = self.__dim
-        num_points = self.__num_points
-        X_without_dummies = np.random.uniform(INF, SUP, (dim, num_points))
-        return np.vstack([np.ones((1, num_points)), X_without_dummies])
-
     @property
     def line(self):
-        """Random line"""
+        """Randomly generated line represented as (w0, w1, w2)."""
         return self.__line
 
     @property
     def positive_points(self):
+        """All points on the positive side of the line."""
         return self.X[:, (self.line.T.dot(self.X) > 0).reshape(-1)]
 
     @property
     def negative_points(self):
+        """All points on the negative side of the line."""
         return self.X[:, (self.line.T.dot(self.X) < 0).reshape(-1)]
 
-    def _line(self):
-        """Creates a line based on p1 and p2."""
-        p1 = np.array(self.__p1)
-        p2 = np.array(self.__p2)
-        return p1 - p2
-
-    def _gram_schmidt(self):
-        """Finds a solution using the Gram-Schmidt process."""
-        u = np.random.uniform(-1, 1, (3, 1))
-        x = np.array([1, *self._line()]).reshape(3, -1)
-        w = u - np.dot(x.T, u) / np.dot(x.T, x) * x
-        return w
-
     def plot(self):
+        """Plots all the points and the line."""
         positive_x = self.positive_points[1, :]
         positive_y = self.positive_points[2, :]
         negative_x = self.negative_points[1, :]
@@ -82,3 +65,23 @@ class Data(object):
         plt.scatter(negative_x, negative_y, marker='x')
         plt.plot(self.p1, self.p2)
         plt.show()
+
+    def _vector(self):
+        """Creates a vector based on p1 and p2."""
+        p1 = np.array(self.__p1)
+        p2 = np.array(self.__p2)
+        return p1 - p2
+
+    def _gram_schmidt(self):
+        """Finds a solution using the Gram-Schmidt process."""
+        u = np.random.uniform(-1, 1, (3, 1))
+        x = np.array([1, *self._vector()]).reshape(3, -1)
+        w = u - np.dot(x.T, u) / np.dot(x.T, x) * x
+        return w
+
+    def _initialize_points(self):
+        """Random points"""
+        dim = self.__dim
+        num_points = self.__num_points
+        X_without_dummies = np.random.uniform(INF, SUP, (dim, num_points))
+        return np.vstack([np.ones((1, num_points)), X_without_dummies])
