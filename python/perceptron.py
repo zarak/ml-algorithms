83c77353fea97fe4cd19215b90ad76c8b3fc202b
Zarak Mahmud <zarak.mahmud@utoronto.ca>
2017-09-17 18:21:43 +0500

    Add methods for gram_schmidt and line

--- python/perceptron.py
+++ python/perceptron.py
@@ -1,6 +1,7 @@
 """Perceptron Learning Algorithm"""
 
 import numpy as np
+from collections import namedtuple
 
 
 __description__ = 'Perceptron Learning Algorithm'
@@ -8,23 +9,34 @@ __author__ = 'Zarak (@z0k)'
 
 
 SUPREMUM = 1
-INFIMUM = 0
+INFIMUM = -1
 DATADIR = '.'
+
+
+Point = namedtuple('Point', ['x1', 'x2'])
 
 
 class Data(object):
     def __init__(self):
-        self.__x0 = np.random.rand()
-        self.__x1 = np.random.rand()
+        self.__p1 = Point(np.random.rand(), np.random.rand())
+        self.__p2 = Point(np.random.rand(), np.random.rand())
 
     @property
-    def f(self):
-        """Target function"""
-        pass
+    def line(self):
+        """Creates a line based on p1 and p2."""
+        p1 = self.__p1
+        p2 = self.__p2
+        return p1 - p2
 
     @property
     def data(self):
         """Random points"""
         pass
+
+    def _gram_schmidt(self):
+        """Finds a solution using the Gram-Schmidt process."""
+        line = np.array([1, *self.line()])
+
+        u = np.
         
 
