5849a97aedff55005ccfec7d0fbb1cd6b9a73837
zarak <zarak.mahmud@utoronto.ca>
2017-10-04 15:59:24 +0500

    Complete fit and predict methods

--- python/linear.py
+++ python/linear.py
@@ -7,6 +7,17 @@ class LinearRegression(object):
         self._w = np.zeros(SHAPE)
         self._iterations = 0
 
-    def fit(self, train, labels):
-        pass
+    @property
+    def w(self):
+        return self._w
 
+    def _activation(self, z):
+        return np.sign(z)
+
+    def fit(self, train, labels):
+        X = train
+        y = labels
+        self._w = y.dot(X.T).dot(np.linalg.inv(X.dot(X.T)))
+
+    def predict(self, test):
+        return self._activation(self._w.dot(test))
