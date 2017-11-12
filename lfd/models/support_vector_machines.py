eab7e5bdcdeccc8871cafe27deb5aa01e9dc47b8
Zarak Mahmud <zarak.mahmud@utoronto.ca>
2017-11-12 21:29:04 +0500

    Initial draft of support vector machines

---
+++ lfd/models/support_vector_machines.py
@@ -0,0 +1,57 @@
+from lfd import synthetic
+import numpy as np
+from quadprog import solve_qp
+
+
+class SVM:
+    def _set_dimensions(self, X):
+        # X should be a matrix of shape (N, 3)
+        assert X.shape[1] == 3
+        self._N = X.shape[0]
+        self._d = X.shape[1] - 1
+
+    def _construct_matrices(self, X, y):
+        self._set_dimensions(X)
+        self._p = np.zeros((self._d + 1, 1))
+        self._c = np.ones((self._N, 1))
+
+        # Components of Q
+        zero = np.zeros((1, 1))
+        zero_d = np.zeros((self._d, 1))
+        eye_d = np.eye(self._d)
+        
+        # Create Q, the quadratic term
+        Q_0 = np.hstack([zero, zero_d.T])
+        Q_1 = np.hstack([zero_d, eye_d])
+        self._Q = np.vstack([Q_0, Q_1])
+
+        # Construct A
+        Y = np.diag(np.squeeze(y))
+        self._A = Y @ X
+
+    @property
+    def w(self):
+        return self._w
+
+    def fit(self, X, y):
+        # Ensure that there is not just one label for all points
+        self._set_dimensions(X)
+        self._construct_matrices(X, y)
+        # Squeeze and transpose to put in appropriate format for solver
+        Q = self._Q + np.eye(3) * 1e-5 # Make Q positive definite
+        A = self._A.T
+        p = np.squeeze(self._p)
+        c = np.squeeze(self._c)
+        print(Q.shape)
+        print(A.shape)
+        print(p.shape)
+        print(c.shape)
+        # Minimize     1/2 u^T Q u - p^T u
+        # Subject to   C.T Au >= c
+        result = solve_qp(Q, p, A, c)[0]
+        self._w = result
+
+    def predict(self, X):
+        # X should be a matrix of shape (N, 3)
+        assert X.shape[1] == 3
+        return np.sign(X @ self.w)
