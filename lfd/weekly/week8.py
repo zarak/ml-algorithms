209e1b1dea514c3913e46ad98509972b6b242979
zarak <zarak.mahmud@utoronto.ca>
2017-11-20 13:38:40 +0500

    Complete question 5; begin question 6

--- lfd/weekly/week8.py
+++ lfd/weekly/week8.py
@@ -27,6 +27,7 @@ def generate_digit2labels(y):
     
 
 def default_params():
+    """Returns the parameter settings for questions 2 through 4"""
     kernel = 'poly'
     Q = 2
     C = 0.01
@@ -74,4 +75,39 @@ def question4():
 
     return np.abs(np.sum(svm_0.n_support_) - np.sum(svm_1.n_support_))
 
+
+def one_versus_five():
+    X_train, X_test, y_train, y_test = load_data()
+    train_idx = np.where((y_train == 5) | ( y_train == 1 ))
+    test_idx = np.where((y_test == 5) | ( y_test == 1 ))
+    # True when the label is 1
+    y_train_1_vs_5 = y_train[(y_train == 5) | ( y_train == 1 )] == 1
+    y_test_1_vs_5 = y_test[(y_test == 5) | ( y_test == 1 )] == 1
+    return X_train[train_idx], X_test[test_idx], y_train_1_vs_5, y_test_1_vs_5
+
+
+def question5():
+    kernel = 'poly'
+    X_train, X_test, y_train, y_test = one_versus_five()
+
+    Q = 2
+    C_values = [0.001, 0.01, 0.1, 1.0]
+    for C in C_values:
+        svm = SVC(kernel=kernel, coef0=1, gamma=1, degree=Q, C=C)
+        svm.fit(X_train, y_train)
+        train_preds = svm.predict(X_train)
+        test_preds = svm.predict(X_test)
+        E_in = np.mean(y_train != train_preds)
+        E_out = np.mean(y_test != test_preds)
+        print(f"Number of support vectors for {C}", np.sum(svm.n_support_))
+        print(f"E_in for {C}", E_in)
+        print(f"E_out for {C}", E_out)
+        
+
+def question6():
+    kernel = 'poly'
+    X_train, X_test, y_train, y_test = one_versus_five()
+    
+    Q_values = range(2, 6)
+    C_values = [0.0001, 0.001, 0.01, 1.0]
 
